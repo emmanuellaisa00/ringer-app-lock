5ba4e069a648a1546d4a7f014e0f812cf298d2f3
emmanuellaisa00 <emmanuellaisa00@users.noreply.github.com>
2026-06-21 07:50:29 +0000

    Fix missing Intent import in LockActivity

--- app/src/main/java/com/example/ringer/LockActivity.kt
+++ app/src/main/java/com/example/ringer/LockActivity.kt
@@ -1,6 +1,7 @@
 package com.example.ringer
 
 import android.content.Context
+import android.content.Intent
 import android.media.AudioManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
